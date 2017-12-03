2edfb35b1ec6e6487cf59e695b41c63270a27619
Nicholas Carignan <nick.carignan@sbcglobal.net>
2017-12-02 17:55:44 -0800

    four descriptive, beautiful, green, sexcessful tests

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -8,86 +8,93 @@ const chaos = require('../lib/chaos');
 
 describe('index.js', () =>{
   describe('bitmapper.writer', () => {
-    test('should create 3 files with transformations and changed file name', done => {
+    test('chaos.chaos should create 3 files with transformations and changed file name', done => {
+      let inputPaths = [
+        `${__dirname}/assets/bitmap.bmp`,
+        `${__dirname}/assets/finger-print.bmp`,
+        `${__dirname}/assets/house.bmp`,
+      ];
+      let transformName = 'chaos';
+
+      let outputPaths = [
+        `${__dirname}/../__test__/dump/bitmap-${transformName}.bmp`,
+        `${__dirname}/../__test__//dump/finger-print-${transformName}.bmp`,
+        `${__dirname}/../__test__/dump/house-${transformName}.bmp`,
+        `${__dirname}/../__test__/dump/non-palette-bitmap-${transformName}.bmp`,
+      ];
+
+      bitmapper.writer(inputPaths, outputPaths, transformName, (error,data) => {
+        expect(data[0].buffer).not.toBeNull();
+        expect(data[1].buffer).not.toBeNull();
+        expect(data[2].buffer).not.toBeNull();
+        done();
+      });
+    });
+    test('greyscale.greyscale should create 3 files with transformations and changed file name', done => {
       let inputPaths = [
         `${__dirname}/assets/bitmap.bmp`,
         `${__dirname}/assets/finger-print.bmp`,
         `${__dirname}/assets/house.bmp`,
       ];
 
-      let outputPaths = 'string standin';
-      let transformName = 'invert';
+      let transformName = 'greyscale';
+      let outputPaths = [
+        `${__dirname}/../__test__/dump/bitmap-${transformName}.bmp`,
+        `${__dirname}/../__test__//dump/finger-print-${transformName}.bmp`,
+        `${__dirname}/../__test__/dump/house-${transformName}.bmp`,
+        `${__dirname}/../__test__/dump/non-palette-bitmap-${transformName}.bmp`,
+      ];
 
       bitmapper.writer(inputPaths, outputPaths, transformName, (error,data) => {
         expect(data[0].buffer).not.toBeNull();
         expect(data[1].buffer).not.toBeNull();
-        console.log(data[2]);
+        expect(data[2].buffer).not.toBeNull();
+        done();
+      });
+    });
+    test('invert.invert should create 3 files with transformations and changed file name', done => {
+      let inputPaths = [
+        `${__dirname}/assets/bitmap.bmp`,
+        `${__dirname}/assets/finger-print.bmp`,
+        `${__dirname}/assets/house.bmp`,
+      ];
+
+      let transformName = 'invert';
+      let outputPaths = [
+        `${__dirname}/../__test__/dump/bitmap-${transformName}.bmp`,
+        `${__dirname}/../__test__//dump/finger-print-${transformName}.bmp`,
+        `${__dirname}/../__test__/dump/house-${transformName}.bmp`,
+        `${__dirname}/../__test__/dump/non-palette-bitmap-${transformName}.bmp`,
+      ];
+
+      bitmapper.writer(inputPaths, outputPaths, transformName, (error,data) => {
+        expect(data[0].buffer).not.toBeNull();
+        expect(data[1].buffer).not.toBeNull();
         expect(data[2].buffer).not.toBeNull();
         // expect(data[1].buffer).not.toBeNull();
         // expect(data[2].buffer).not.toBeNull();
         done();
       });
     });
-    // test('should return error callback if a filepath is bad', done => {
-    //   let filePaths = [
-    //     `${__dirname}/assets/bitmaaaaaaaap.bmp`,
-    //     `${__dirname}/assets/finger-print.bmp`,
-    //     `${__dirname}/assets/house.bmp`,
-    //     `${__dirname}/assets/non-palette-bitmap.bmp`,
-    //   ];
-    //   bitmapper.parser(filePaths, (error, data) => {
-    //     expect(error).not.toBeNull();
-    //     done();
-    //   });
-    // });
-  // });
+    test('should return error callback if a filepath is bad', done => {
+      let inputPaths = [
+        `${__dirname}/assets/bitmapp.bmp`,
+        `${__dirname}/assets/finger-print.bmp`,
+        `${__dirname}/assets/house.bmp`,
+      ];
 
-  // describe('chaos.chaos.js', () => {
-  //   test('modify colors to be different', done => {
-  //     let filePaths = [
-  //       `${__dirname}/assets/bitmap.bmp`,
-  //       `${__dirname}/assets/finger-print.bmp`,
-  //       `${__dirname}/assets/house.bmp`,
-  //       `${__dirname}/assets/non-palette-bitmap.bmp`,
-  //     ];
-  //     let objectArray = bitmapper.parser(filePaths, (error, data) => {
-  //       done();});
-  //
-  //     console.log('objectArray: ', objectArray);
-  //     chaos.chaos(objectArray, (error, data) => {
-  //       expect(error).toBeNull();
-  //       expect(data[0].color).not.toBe(objectArray[0].color); //making sure color changed;
-  //       expect(data[1].color).not.toBe(objectArray[1].color); //making sure color changed;
-  //       expect(data[2].color).not.toBe(objectArray[2].color); //making sure color changed;
-  //       expect(data[3].color).not.toBe(objectArray[3].color); //making sure color changed;
-  //     });
-  //   });
+      let transformName = 'invert';
+      let outputPaths = [
+        `${__dirname}/../__test__/dump/bitmap-${transformName}.bmp`,
+        `${__dirname}/../__test__//dump/finger-print-${transformName}.bmp`,
+        `${__dirname}/../__test__/dump/house-${transformName}.bmp`,
+        `${__dirname}/../__test__/dump/non-palette-bitmap-${transformName}.bmp`,
+      ];
 
+      bitmapper.writer(inputPaths, outputPaths, transformName, (error,data) => {
+        expect(error).not.toBeNull();
+        done();
+      });
+    });
   });
-
-  // describe('bitmapper.createNewFiles', () => {
-  //   test('should output four new files in dump folder', done => {
-  //     let inputFilePaths = [
-  //       `${__dirname}/assets/bitmap.bmp`,
-  //       `${__dirname}/assets/finger-print.bmp`,
-  //       `${__dirname}/assets/house.bmp`,
-  //       `${__dirname}/assets/non-palette-bitmap.bmp`,
-  //     ];
-  //     let outputFilePaths = [
-  //       `${__dirname}/dump/bitmap2.bmp`,
-  //       `${__dirname}/dump/finger-print2.bmp`,
-  //       `${__dirname}/dump/house2.bmp`,
-  //       `${__dirname}/dump/non-palette-bitmap2.bmp`,
-  //     ];
-  //     let transformer = 'undefined function'; //put name of function here
-  //     bitmapper.createNewFiles(inputFilePaths, outputFilePaths, transformer, (error,data) => {
-  //       expect(typeof data[0]).toBe('object');
-  //       expect(typeof data[1]).toBe('object');
-  //       expect(typeof data[2]).toBe('object');
-  //       expect(typeof data[3]).toBe('object');
-  //       done();
-  //     });
-  //   });
-  //
-  // });
 });
