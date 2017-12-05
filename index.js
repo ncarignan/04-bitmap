f29f87ad66ab46d3eea380aff6021aadd05cf825
SethDonohue <donohue.seth@gmail.com>
2017-12-05 15:30:37 -0800

    code cleanup

--- index.js
+++ index.js
@@ -3,22 +3,16 @@
 const greyscale = require('./lib/greyscale.js');
 const invert = require('./lib/invert.js');
 const chaos = require('./lib/chaos.js');
-
-
 const bitmap = require('./lib/bitmap.js');
 const fs = require('fs');
-
 const bitmapper = module.exports = {};
 
 
 let inputPaths = [];
 inputPaths.push(process.argv[2]);
-
 let outputPaths = [];
 outputPaths.push(process.argv[3]);
-
 let transformName = process.argv[4];
-
 
 
 bitmapper.writer = (inputPaths, outputPaths, transformName, callback) =>{
